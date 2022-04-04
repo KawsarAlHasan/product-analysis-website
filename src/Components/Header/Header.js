681f6fba8a92fd5a6886023e1e3930800b2ce062
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-04-04 18:22:28 +0600

    added react Routes

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <a href="home">HOME</a>    
+                <a href="reviews">REVIEWS</a>    
+                <a href="dashboard">DASHBOARD</a>   
+                <a href="blogs">BLOGS</a> 
+                <a href="about">ABOUT</a>    
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import img from './images/Funny-404-Pages.gif';
+import './NotFound.css'
+
+const NotFound = () => {
+    return (
+        <div>
+            <img src={img} alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
