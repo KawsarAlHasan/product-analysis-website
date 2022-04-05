26b702545e9b1fdf1562bb3e5bc9016cf810062c
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-04-05 11:19:56 +0600

    Answer to question

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -4,9 +4,19 @@ const Blogs = () => {
     return (
         <div className='text-center'>
             <p className='font-semibold text-xl'>Answer to the Question No.1</p>
-            <p className='font-medium'>Ans: This is Answer</p>
+            <p className='font-medium'>Ans: If a child component at nth level require a property from a parent component at <br />
+            any level, the information needs to be passed one level by level through props. In an <br />
+            application with lot of nested components, it is difficult.<br /> 
+            Context API helps to directly send an information from a parent component to a child <br />
+            component at any level.</p>
+
             <p className='font-semibold text-xl mt-5'>Answer to the Question No.2</p>
-            <p className='font-medium'>Ans: This is Answer2</p>
+            <p className='font-medium'>Ans: Semantic HTML or semantic markup is HTML that introduces meaning to the web <br />
+            page rather than just presentation. For example, a "p" tag indicates that the enclosed <br /> 
+            text is a paragraph. This is both semantic and presentational because people know what <br /> 
+            paragraphs are, and browsers know how to display them. On the flip side of this equation, <br /> 
+            tags such as "b" and "i" are not semantic. They define only how the text should look <br /> 
+            (bold or italic), and don't provide any additional meaning to the markup.</p>
         </div>
     );
 };
